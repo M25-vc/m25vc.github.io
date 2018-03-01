4fac232390d2b3895479c52fb960f9e8073fb332
m25vc <36930133+m25vc@users.noreply.github.com>
2018-03-01 14:31:15 -0600

    init

---
+++ js/blog.js
@@ -0,0 +1,30 @@
+$(function () {
+	var $content = $('#m25Blog');
+	var data = {
+		rss_url: 'https://medium.com/feed/m25-vc'
+	};
+	$.get('https://api.rss2json.com/v1/api.json', data, function (response) {
+		if (response.status == 'ok') {
+			var output = '';
+			$.each(response.items, function (k, item) {
+				var visibleSm;
+				if(k < 2){
+					visibleSm = '';
+				} else {
+					visibleSm = ' visible-sm';
+				}
+				output += '<div class="card" style="width: 20rem;">';
+				var tagIndex = item.description.indexOf('<img'); 
+				var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; 
+				var srcStart = srcIndex + 5; 
+				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; 
+				var src = item.description.substring(srcStart, srcEnd);
+				output += '<a class = "" href="'+ item.link + '"><img class="card-img-top" src="' + src + '" alt="Card image cap"><div class="card-block pt-2 px-1">';
+				output += '<h5 class="card-title">' +item.title + '</a></h5>';
+				output += '</div></div>';
+				return k < 2;
+			});
+			$content.html(output);
+		}
+	});
+});
---
+++ js/portfolio.js
@@ -0,0 +1,15 @@
+function getportfolioDetails(portfolio) {
+	return `<a href="${portfolio.url}" target="_blank">
+              <img class="item ${portfolio.class} isotope-item" src="img/portfolio/${portfolio.img}.jpg" alt="${portfolio.img}" style="position: absolute; left: 0px; top: 0px; margin:20px;">
+            </a>`;
+
+}
+function showportfolio(portfolioDetails) {
+	document.querySelector(".portco-thumb").innerHTML += portfolioDetails;
+
+}
+portfolio.map(getportfolioDetails).forEach(showportfolio);
+
+
+
+
