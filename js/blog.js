bbe16af769c2f39fb8e7172807f3d24cbdb357b9
m25vc <36930133+m25vc@users.noreply.github.com>
2018-03-28 12:21:58 -0500

    blog remove

--- js/blog.js
+++ js/blog.js
@@ -5,11 +5,6 @@ $(function () {
 	};
 	$.get('https://api.rss2json.com/v1/api.json', data, function (response) {
 			var output = '';
-			output += '<div class="card card-1" style="width: 20rem;">';
-				output += '<a class = "" href="https://blog.midweststartups.com/adding-value-to-startups-club-m25-summit-2-0-3a7acaf10161"><img class="card-img-top" src="https://cdn-images-1.medium.com/max/2000/1*ugGAGunUM0idKMkzccg6aw.jpeg" alt="Card image cap"><div class="card-block pt-2 px-1">';
-				output += '<h5 class="card-title">Adding Value to Startups: Club M25 Summit 2.0</a></h5>';
-				output += '</div></div>';
-
 		if (response.status == 'ok') {
 			$.each(response.items, function (k, item) {
 				var visibleSm;
