bf1118e66279bca4171e218e02094ecb4aa048e1
itsabhinaya <>
2018-09-15 08:40:59 -0500

    midwest blog

--- js/blog.js
+++ js/blog.js
@@ -28,3 +28,36 @@ $(function () {
 		}
 	});
 });
+
+
+
+$(function () {
+	var $content = $('#midwestStartupsBlog');
+	var data = {
+		rss_url: 'https://medium.com/feed/Midwest-Startups'
+	};
+	$.get('https://api.rss2json.com/v1/api.json', data, function (response) {
+			var output = '';
+		if (response.status == 'ok') {
+			$.each(response.items, function (k, item) {
+				var visibleSm;
+				if(k < 2){
+					visibleSm = '';
+				} else {
+					visibleSm = ' visible-sm';
+				}
+				output += '<div class="card card-1" style="width: 20rem;">';
+				var tagIndex = item.description.indexOf('<img'); 
+				var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; 
+				var srcStart = srcIndex + 5; 
+				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; 
+				var src = item.description.substring(srcStart, srcEnd);
+				output += '<a class = "" href="'+ item.link + '"><img class="card-img-top" src="' + src + '" alt="Card image cap"><div class="card-block pt-2 px-1">';
+				output += '<h5 class="card-title">' +item.title + '</a></h5>';
+				output += '</div></div>';
+				return k < 2;
+			});
+			$content.html(output);
+		}
+	});
+});
