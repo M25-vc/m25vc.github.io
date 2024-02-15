9c807263fc698396c6b65995f327db58f37d34bf
Abhinaya Konduru <theabhinaya@gmail.com>
2024-02-15 12:36:22 -0600

    summit page index js issue for nav links

---
+++ js/summit.js
@@ -0,0 +1,33 @@
+$(function(){ 
+     var navMain = $(".navbar-collapse"); // avoid dependency on #id
+     // "a:not([data-toggle])" - to avoid issues caused
+     // when you have dropdown inside navbar
+     navMain.on("click", "a:not([data-toggle])", null, function () {
+         navMain.collapse('hide');
+     });
+ });
+
+var $item = $('.carousel-item');
+
+var $numberofSlides = $('.carousel-item').length;
+var $currentSlide = Math.floor((Math.random() * $numberofSlides));
+
+$('.carousel-indicators li').each(function(){
+  var $slideValue = $(this).attr('data-slide-to');
+  if($currentSlide == $slideValue) {
+    $(this).addClass('active');
+    $item.eq($slideValue).addClass('active');
+  } else {
+    $(this).removeClass('active');
+    $item.eq($slideValue).removeClass('active');
+  }
+});
+
+$('.carousel').carousel({
+  interval: 5000,
+  pause: "false"
+});
+
+$('#form_sub').hide();
+$('[data-toggle="tooltip"]').tooltip(); 
+
